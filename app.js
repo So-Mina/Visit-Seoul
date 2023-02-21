7c5ecd6bf068d5308f84a1a6619cc46ffa8b73bb
Soizic Cistac <soizic.cistac@gmail.com>
2023-02-21 10:23:58 +0100

    add places routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const hbs = require('hbs')
+const path = require('path')
+
 
 const app = express()
 
@@ -10,6 +12,8 @@ require('./db')
 app.set('view engine', 'hbs')
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+
 
 const indexRoutes = require('./routes/index.routes')
 app.use('/', indexRoutes)
